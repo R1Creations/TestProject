cd3cf4d8adefb7b83573bce6d689c227e7cb426c
Aroan <aroan1226@yahoo.com>
2015-11-15 22:55:05 +0530

    Test

--- TestProject/TestProject/Controllers/HomeController.cs
+++ TestProject/TestProject/Controllers/HomeController.cs
@@ -8,11 +8,27 @@ namespace TestProject.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your A SP.NET MVC application.";
+
+            for (int i = 0; i < 11; i++)
+            {
+                var test = Number();
+                var test1 = test.Aggregate((x, y) => x + y);
+            }
 
             return View();
+        }
+
+        private IEnumerable<int> Number()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return i;
+            }
+            var test= "Aroan";
         }
 
         public ActionResult About()
